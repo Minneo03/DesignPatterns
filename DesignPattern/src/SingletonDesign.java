dc27617e71a6bd8ed58eb1d407d3d385646b13fc
RyanMinneo <minneor@go.stockton.edu>
2023-03-09 16:31:39 -0500

    Singleton Creation
    Created a singleton type of class and called it from a main method in
    the SingletonDesignTester.

--- DesignPattern/src/SingletonDesign.java
+++ DesignPattern/src/SingletonDesign.java
@@ -1,4 +1,56 @@
-
-public class SingletonDesign {
-
+/**
+ * This class creates a singletonObj and only allows one instance of the object to occur by creating a private constructor and a public static getInstance method.
+ * 
+ * @author Ryan Minneo
+ * @version - 1.0
+ */
+public class SingletonDesign 
+{
+	private static SingletonDesign singletonObj = null;
+	private int numbah;
+	
+	/**
+	 * Private constructor for the singletonObj, this will only be able to be called inside of this class.
+	 */
+	private SingletonDesign()
+	{
+		this.numbah = 0;
+	}
+	
+	
+	/**
+	 * This method checks to see if there is an instance of the singletonObj created yet. If there is not a singletonObj created yet then it will call the private constructor above. If there already is a singletonObj, then the second object will be set to the same instance.
+	 * 
+	 * @return singletonObj - the only instance of a SingletonDesign
+	 */
+	public static SingletonDesign getInstance()
+	{
+		if (singletonObj == null)
+		{
+			singletonObj = new SingletonDesign();
+		}
+		
+		return singletonObj;
+	}
+	
+	
+	/**
+	 * Classic getter, retrieves the numbah variable from the instance of SingletonDesign
+	 * 
+	 * @return numbah - the numbah
+	 */
+	public int getNumbah()
+	{
+		return numbah;
+	}
+	
+	/**
+	 * Classic setter, sets the numbah variable from the instance to the int inputted into the method.
+	 * @param x - the inputted number which will become the new numbah variable for the instance.
+	 */
+	public void setNumbah(int x)
+	{
+		numbah = x;
+	}
+	
 }
--- DesignPattern/src/SingletonDesignTester.java
+++ DesignPattern/src/SingletonDesignTester.java
@@ -1,4 +1,25 @@
-
-public class SingletonDesignTester {
-
+/**
+ * Just testing to see how Singletons work, this class simply contains the main method used to test for if the singleton works.
+ * 
+ * @author Ryan Minneo
+ * @version 1.0
+ */
+public class SingletonDesignTester 
+{
+	public static void main(String[] args)
+	{
+		SingletonDesign firstInst = SingletonDesign.getInstance();
+		SingletonDesign secondInst;
+		
+		firstInst.setNumbah(30);
+		
+		System.out.println(firstInst.getNumbah());
+		
+		secondInst = SingletonDesign.getInstance();
+		
+		secondInst.setNumbah(15);
+		
+		System.out.println(firstInst.getNumbah());
+		System.out.println(secondInst.getNumbah());
+	}
 }
